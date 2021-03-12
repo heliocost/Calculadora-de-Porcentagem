dc44b9644884f1bcf2ed199604ffa31dbce081c0
Helio Costa <helioocosta18@gmail.com>
2021-03-12 00:00:47 -0300

    Calculadora de Porcentagem

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+
+function porcentagem () {
+
+
+  var porcentagem =  parseFloat(document.getElementById("porcent").value);
+
+  var valor = parseFloat(document.getElementById("valor_total").value);
+
+  var valorTotal = document.getElementById("total");
+
+  if (valor > 0  && porcentagem > 0) {
+
+    var result = ((porcentagem / 100) * valor);
+
+   
+  
+    document.getElementById("porcent").value = null;
+    document.getElementById("valor_total").value = null;
+
+
+
+    valorTotal.innerHTML = `${porcentagem} % de ${valor} = ${result.toFixed(0)}`;
+    
+  } else {
+
+    valorTotal.innerHTML = "Nenhum cálculo.";
+
+
+  }
+
+
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div id="container" >
+
+                <div id="primeirotitlo">
+                  Calculadora de Porcentagem
+                  </div>
+                <hr/>
+
+                <div id="container_dois">
+                  
+                 <span> Quanto é </span> <input id="porcent" type="number" placeholder=" % " />
+                 <span> de </span> <input id="valor_total" type="number" placeholder="valor"/> ?
+                </div>
+
+                <div>
+                  <h1 id="total">Nenhum cálculo.</h1>
+                </div>
+
+                <hr/>
+              <div>
+                <button id="botao_result" onClick={()=>{porcentagem()}}>Calcular</button>
+              </div>
+
+        </div>
+
+
       </header>
     </div>
   );
